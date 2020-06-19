6c68002fb17c32eb68e6a97192e91ad8bb159b96
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2020-06-19 16:09:38 +0530

    tail pointer removed

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
-class sortedLinkedList {
+class SortedLinkedList {
     constructor() {
         this.head;
-        this.tail;
         this.length=0;
     }
     add(value) {
         let newNode = { value: value, next: undefined };
-        if (this.head === undefined && this.tail === undefined) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-            if (this.tail.value <= value) {
-                this.tail.next = newNode;
-                this.tail = this.tail.next;
-                this.length++;
-                return;
-            }
-            if (this.head.value > value) {
-                let tempNode = this.head;
-                this.head = newNode;
-                newNode.next = tempNode;
-            }
-            let currentNode = this.head;
-            while (currentNode.next.value <= value) {
-                currentNode = currentNode.next;
-            }
-            let tempNode = currentNode.next;
-            currentNode.next = newNode;
-            newNode.next = tempNode;
+        if (this.head=== undefined ) {
+            newNode.next=this.head;
+              this.head=newNode;
         }
+        else if(this.head.value > newNode.value){
+            let tempNode=this.head;
+            this.head=newNode;
+            newNode.next=tempNode;
+        }else {
+            let current = this.head;
+                while (current.next !== undefined && current.next.value <= value) {
+                current = current.next;
+            }
+                newNode.next = current.next;
+                current.next = newNode;
+            }
+
         this.length++;
         return;
     }
@@ -39,7 +32,7 @@ class sortedLinkedList {
         let linkedListString = "[";
         let currentNode = this.head;
         while (currentNode.next !== undefined) {
-            linkedListString += currentNode.value.toString() + ",";
+            linkedListString += currentNode.value.toString() + ", ";
             currentNode = currentNode.next;
         }
         linkedListString += currentNode.value.toString();
@@ -47,48 +40,44 @@ class sortedLinkedList {
         return linkedListString;
     }
     remove(value) {
-        if (this.head.value === value) {
-            this.head = this.head.next;
+        if (this.head.value=== value ){
+            this.head=this.head.next;
             this.length--;
             return true;
         }
-        let currentNode = this.head;
-        while (currentNode.next.value !== value) {
-            currentNode = currentNode.next;
-            if (this.tail === currentNode) {
-                if (currentNode.value !== value) {
-                    return false;
-                }
-            }
-        }
-        if (currentNode.next.next === undefined) {
-            //tail
-            currentNode.next = undefined;
-            this.tail = currentNode;
-            this.length--;
-            return true;
-        } else if (currentNode.next.next !== undefined) {
-            //not tail
-            let value = currentNode.next.value;
-            currentNode.next = currentNode.next.next;
-            this.length--;
-            return true;
-        }
+       let currentNode=this.head;
+        let previousNode;
+       while(currentNode.value!==value){
+           previousNode=currentNode;
+           currentNode=currentNode.next;
+           if(currentNode===undefined){
+               return false;
+           }
+       }
+       previousNode.next=currentNode.next;
+       this.length--;
+       return true;
     }
 }
-let list1 = new sortedLinkedList();
+let list1 = new SortedLinkedList();
 list1.add(2);
-list1.add(3);
-list1.add(4);
-list1.add(7);
-list1.add(5);
-list1.add(1);
-console.log(list1.toString());
+ list1.add(3);
+   list1.add(4);
+    list1.add(7);
+    list1.add(5);
+    list1.add(1);
+list1.add(6);
+list1.add(9);
+list1.add(8);
+ console.log(list1.toString());
 console.log(list1.remove(1));
-console.log(list1.remove(7));
+console.log(list1.toString());
+ console.log(list1.remove(7));
+ console.log(list1.toString());
+console.log(list1.remove(9));
 console.log(list1.toString());
 console.log(`${list1}`);
-let scores = new sortedLinkedList();
+let scores = new SortedLinkedList();
 scores.add(54);
 scores.add(33);
 scores.add(91);
@@ -97,7 +86,17 @@ console.log(`${scores}`); // Should print [33, 44, 54, 91];
 console.log(scores.remove(44)); // Should print true
 console.log(`${scores}`); // Should print [33, 54, 91];
 console.log(scores.remove(70)); // Should print false
-let list2 = new sortedLinkedList();
+let list2 = new SortedLinkedList();
 list2.add(2);
 console.log(list2.remove(2));
 console.log(`${list2}`);
+let list3 = new SortedLinkedList();
+list3.add(1);
+list3.add(1);
+list3.add(2);
+list3.add(2);
+list3.add(3);
+list3.add(3);
+console.log(`${list3}`);
+list3.remove(3);
+console.log(`${list3}`);
